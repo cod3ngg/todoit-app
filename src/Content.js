ef2fde5b35fd4b9194714525605c651ea9c0856a
cod3ngg <150222307+cod3ngg@users.noreply.github.com>
2024-12-06 22:12:22 +0800

    New app with react components

---
+++ src/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <div className="mainBody">
+            <h1>ToDoIT</h1>
+            <p>Unique To-Do-List app being developed by Lorenz Lone.</p>
+        </div>
+    )
+}
+
+export default MainContent;
