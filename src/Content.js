01363d6b29f999408f47b58004696eaf45d433ae
Lorenz <150222307+cod3ngg@users.noreply.github.com>
2024-12-06 15:02:14 +0000

    Added image logo in the content

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+
 function MainContent() {
     return (
         <div className="mainBody">
             <h1>ToDoIT</h1>
             <p>Unique To-Do-List app being developed by Lorenz Lone.</p>
+            <img src="/assets/To Do iT_image.png" width="300px" />
         </div>
     )
 }
